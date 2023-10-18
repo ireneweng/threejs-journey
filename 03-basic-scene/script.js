b6935149bfd4e604da05cfb3cf99fadd3f7fff1c
Ran Weng <ireneylzr@gmail.com>
2023-10-18 15:52:22 -0400

    fix lesson 3 bug

--- 03-basic-scene/script.js
+++ 03-basic-scene/script.js
@@ -15,7 +15,7 @@ const sizes = {
 
 // camera
 // field of view, aspect ratio
-const camera = new THREE.PerspectiveCamera(75, sizes.width, sizes.height)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
 
