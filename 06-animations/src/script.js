9b6897305aad6023d8fa570b9864a34a20997068
Ran Weng <ireneylzr@gmail.com>
2023-10-19 14:07:08 -0400

    update lesson 6 file

--- 06-animations/src/script.js
+++ 06-animations/src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import gsap from 'gsap'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -29,3 +30,44 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
+
+// Time
+let time = Date.now()
+
+// Clock
+const clock = new THREE.Clock()
+
+// gsap.to(mesh.position, {duration: 1, delay: 1, x: 2})
+// gsap.to(mesh.position, {duration: 1, delay: 2, x: 0})
+
+// Animations
+const tick = () => {
+    // // Time
+    // const current_time = Date.now()
+    // const delta_time = current_time - time
+    // time = current_time
+
+    // Clock
+    const elapsed_time = clock.getElapsedTime()
+
+    // // update objects
+    // mesh.position.x += 0.01
+    // mesh.position.y += 0.01
+    
+    // // using delta
+    // mesh.rotation.y += 0.001 * delta_time
+
+    // // using clock
+    // mesh.rotation.y = elapsed_time * Math.PI * 2
+
+    // fun with math functions
+    camera.position.x = Math.cos(elapsed_time)
+    camera.position.y = Math.sin(elapsed_time)
+    camera.lookAt(mesh.position)
+
+    // render on each frame
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(tick)
+}
+
+tick()
