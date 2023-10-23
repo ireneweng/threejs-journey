edd05bcc46959343148239d9e2af0794e8fc6305
Ran Weng <ireneylzr@gmail.com>
2023-10-23 13:32:41 -0400

    add lesson 11 files

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,20 @@
 // Update for the current lesson
-const lessonFolder = '10-debug-ui'
-
-const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
+const lessonFolder = "11-textures"
+const isCodeSandbox =
+  "SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env
 
 export default {
-    // Update for the current lesson
-    root: `${lessonFolder}/src/`,
-    publicDir: `${lessonFolder}/static/`,
-    base: './',
-    server:
-    {
-        host: true,
-        open: !isCodeSandbox // Open if it's not a CodeSandbox
-    },
-    build:
-    {
-        outDir: '../dist',
-        emptyOutDir: true,
-        sourcemap: true
-    }
+  // Update for the current lesson
+  root: `${lessonFolder}/src`,
+  publicDir: `../static`,
+  base: "./",
+  server: {
+    host: true,
+    open: !isCodeSandbox, // Open if it's not a CodeSandbox
+  },
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    sourcemap: true,
+  },
 }
