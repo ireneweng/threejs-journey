90dac13485dde68f820f276edef74dfbd1d8f39f
Ran Weng <ireneylzr@gmail.com>
2023-11-02 16:19:38 -0400

    update deploy files

--- 14-lights/vite.config.js
+++ 14-lights/vite.config.js
@@ -1,18 +1,17 @@
-const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
+const isCodeSandbox =
+    "SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env
 
 export default {
-    root: 'src/',
-    publicDir: '../static/',
-    base: './',
-    server:
-    {
+    root: "./",
+    publicDir: "static/",
+    base: "./",
+    server: {
         host: true,
-        open: !isCodeSandbox // Open if it's not a CodeSandbox
+        open: !isCodeSandbox, // Open if it's not a CodeSandbox
     },
-    build:
-    {
-        outDir: '../dist',
+    build: {
+        outDir: "dist",
         emptyOutDir: true,
-        sourcemap: true
-    }
+        sourcemap: true,
+    },
 }
