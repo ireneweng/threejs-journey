ee35ef472ff554a5d131940d7b97eee9b21509d7
Ran Weng <ireneylzr@gmail.com>
2024-01-26 15:36:57 -0500

    update lesson 41 file

--- 41-first-react-application/lesson-project/src/People.js
+++ 41-first-react-application/lesson-project/src/People.js
@@ -1,14 +1,53 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function People() {
     const [people, setPeople] = useState([
-        { name: "irene" },
-        { name: "tim" },
-        { name: "mavis" },
+        { id: 1, name: "irene" },
+        { id: 2, name: "tim" },
+        { id: 3, name: "mavis" },
     ])
+
+    const getPeople = async () => {
+        // const request = fetch("https://jsonplaceholder.typicode.com/users")
+        // request.then((response) => {
+        //     const parse = response.json()
+        //     parse.then((result) => setPeople(result)
+        // })
+
+        // simplification with chaining
+        // fetch("https://jsonplaceholder.typicode.com/users")
+        //     .then((response) => {
+        //         return response.json()
+        //     })
+        //     .then((result) => {
+        //         setPeople(result)
+        //     })
+
+        // cleaner syntax
+        // fetch("https://jsonplaceholder.typicode.com/users")
+        //     .then((response) => response.json())
+        //     .then((result) => setPeople(result)
+
+        // asynchronous solution
+        const response = await fetch(
+            "https://jsonplaceholder.typicode.com/users"
+        )
+        const result = await response.json()
+        setPeople(result)
+    }
+
+    useEffect(() => {
+        getPeople()
+    }, [])
+
     return (
         <div>
             <h2>People</h2>
+            <ul>
+                {people.map((person) => {
+                    return <li key={person.id}>{person.name}</li>
+                })}
+            </ul>
         </div>
     )
 }
